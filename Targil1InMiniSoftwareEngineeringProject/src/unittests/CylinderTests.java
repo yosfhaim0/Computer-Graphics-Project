ec4d3d946c7c37d5cdf9c00d111425fd8fbc3bb6
yosef haim <yosfhaim@DESKTOP-QHEC9FF>
2021-03-13 19:00:03 +0200

    0111

--- Targil1InMiniSoftwareEngineeringProject/src/unittests/CylinderTests.java
+++ Targil1InMiniSoftwareEngineeringProject/src/unittests/CylinderTests.java
@@ -26,17 +26,25 @@ public class CylinderTests {
 		//the point Contained low base
 		Point3D p1=new Point3D(0.5,0.5,0);
 		Vector v1= c.getNormal(p1);
-		assertEquals("Bad normal to the low base of Cylinder",new Vector(0,0,1),v1);
+		assertEquals("Bad normal to the low base of Cylinder1",new Vector(0,0,1),v1);
+		
+		Point3D p7=new Point3D(0.24,-0.69,0);
+		Vector v7= c.getNormal(p7);
+		assertEquals("Bad normal to the low base of Cylinder2",new Vector(0,0,1),v7);
         // ============ Equivalence Partitions Tests ==============
 		//the point Contained the up base
 		Point3D p2=new Point3D(0.5,0.5,1);
 		Vector v2= c.getNormal(p2);
-		assertEquals("Bad normal to the up base of Cylinder",new Vector(0,0,1),v2);
+		assertEquals("Bad normal to the up base of Cylinder1",new Vector(0,0,1),v2);
+		
+		Point3D p6=new Point3D(0.44,0.3,1);
+		Vector v6= c.getNormal(p6);
+		assertEquals("Bad normal to the up base of Cylinder2",new Vector(0,0,1),v6);
         // ============ Equivalence Partitions Tests ==============
 		//the point Contained the sides
 		Point3D p3=new Point3D(1,0,0.5);
 		Vector v3= c.getNormal(p3);
-		assertEquals("Bad normal to the sides of Cylinder",new Vector(1,0,0),v3);
+		assertEquals("Bad normal to the sides of Cylinder1",new Vector(1,0,0),v3);
         // =============== Boundary Values Tests ==================
 		//in case of point on Boundary the vector vertical the base of the cylinder
 		Point3D p4=new Point3D(1,0,0);
@@ -48,14 +56,6 @@ public class CylinderTests {
 		Vector v5= c.getNormal(p5);
 		assertEquals("Bad normal Boundary Values2",new Vector(0,0,1),v5);
 		
-		//Point3D p6=new Point3D(0,0.5,1);
-		//Vector v6= c.getNormal(p6);
-		//assertEquals("Bad normal Boundary Values2",new Vector(0,0,1),v6);
-		
-//		Cylinder c1=new Cylinder(new Ray(new Point3D(-6.59431,0.90272,-2.22848),new Vector(9.01,-3.59,6)),1,11.404744);
-//		Point3D p6=new Point3D(-1.32,-0.48,2);
-//		Vector v6= c1.getNormal(p6);
-//		assertEquals("Bad normal Boundary Values3",new Vector(0.1158903146,0.7677733345,0.6301535859),v6);
 	}
 	
 
--- Targil1InMiniSoftwareEngineeringProject/src/unittests/TubeTests.java
+++ Targil1InMiniSoftwareEngineeringProject/src/unittests/TubeTests.java
@@ -23,22 +23,15 @@ public class TubeTests {
 		
 		Tube t=new Tube(new Ray(new Point3D(0,0,0),new Vector(0,0,1)),1);
 		//base case
-		assertEquals("Bad normal to Tube", new Vector(1, 0, 0), t.getNormal(new Point3D(1,0,0)));
+		assertEquals("Bad normal to Tube1", new Vector(1, 0, 0), t.getNormal(new Point3D(1,0,0)));
 		//more complicated
 		//the intersection whit (3,2,0)vector came out form the center AXIS
-		double x=Math.sqrt(13);
-		Vector v=t.getNormal(new Point3D(0.83,0.55,0));
-		assertEquals("Bad normal to Tube",new Vector(3d/x, 2d/x, 0),v);
-		//coplicatest
-		//interscetion whit the vector(1.41,1.41,0) came out for z=1
 		Tube t1=new Tube(new Ray(new Point3D(0,0,0),new Vector(0,0,1)),2);
-		Vector v1=t1.getNormal(new Point3D(1.66,1.11,0));
-		assertEquals("Bad normal to Tube",new Vector(3d/x, 2d/x, 0),v1);
-		/*
-//		//MORE
-//		//interscetion whit the vector(1.41,1.41,0) came out for z=1
-//		Vector v2=t1.getNormal(new Point3D(1.41,1.41,1));
-//		double y=0.7071067811;
+		//Intersection whit the vector(1.41,1.41,0) came out for z=1
+		Vector v2=t1.getNormal(new Point3D(1.41,1.41,1));
+		double y=0.7071067811865475;
+		assertEquals("Bad normal to Tube3",new Vector(y, y, 0),v2);
+		
 //		chackIfEcual(new Vector(y,y,0),v2);
 //	}
 //	public static void chackIfEcual(Vector v1,Vector v2) {
@@ -54,4 +47,4 @@ public class TubeTests {
 //		assertEquals("Bad normal to Tube", z1,z1,0.01);
 //	*/}
 
-}
+}}
