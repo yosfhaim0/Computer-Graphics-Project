d31f800aae4abeefc298c45ab7b30ef44af371fb
alexandre lejdstrom <yehouda.lejdstrom@gmail.com>
2021-04-27 09:46:02 +0300

    reviewed the javaDocs

--- Targil1InMiniSoftwareEngineeringProject/src/elements/Camera.java
+++ Targil1InMiniSoftwareEngineeringProject/src/elements/Camera.java
@@ -6,127 +6,129 @@ package elements;
 import primitives.*;
 
 /**
- * present camera, point of view in three diminution 
- * @author yosefHaim
+ * present camera, point of view in three diminution
+ * 
+ * @author yosefHaim <br>
+ *         JavaDocs reviewed by Alexandre
  *
  */
 public class Camera {
-	/**
-	 * location Point3d of Camera
-	 */
-	private Point3D locationPoint3d;
-	/**
-	 * vector Right Hand Coordinate System
-	 */
-	private Vector vTo, vUp, vRight;
-	/**
-	 * distance between camera and view plane
-	 * width of view plane
-	 * height of view plane
-	 */
-	private double distance = 0, width, height;
+    /**
+     * location of the Camera
+     */
+    private Point3D locationPoint3d;
+    /**
+     * vector Right Hand Coordinate System
+     */
+    private Vector vTo, vUp, vRight;
+    /**
+     * distance between camera and view plane width of view plane height of view
+     * plane
+     */
+    private double distance = 0, width, height;
 
-	/**
-	 * ctor for camera the ctor get to vector up and to and generate the right
-	 * vector
-	 * 
-	 * @param locatPoint point3d
-	 * @param vectorTo   vector
-	 * @param vectorUp   vector
-	 */
-	public Camera(Point3D locatPoint, Vector vectorTo, Vector vectorUp) {
-		// if those vector are not orthogonal they throw exception form crossProduct
-		// function
-		if(vectorTo.dotProduct(vectorUp)!=0)
-			throw new RuntimeException("erorr!, the vector of camera not orthgonal!!");
-		vRight = vectorTo.crossProduct(vectorUp).normalize();
-		vUp = vectorUp.normalized();
-		vTo = vectorTo.normalized();
-		locationPoint3d = locatPoint;
+    /**
+     * camera constructor: receive two orthogonal vectors and build a third one
+     * 
+     * @param locatPoint our point of view
+     * @param vectorTo   pointing where we look
+     * @param vectorUp   up to our looking direction
+     * 
+     * @throws IllegalArgumentException if vectorUp and vectorTo are not orthogonal
+     */
+    public Camera(Point3D locatPoint, Vector vectorTo, Vector vectorUp) {
+	// if vTo and vUp are not orthogonal
+	if (vectorTo.dotProduct(vectorUp) != 0) {
+	    throw new IllegalArgumentException("Error !, vTo and vUp are not orthogonal");
+	}
+	vRight = vectorTo.crossProduct(vectorUp).normalize();
+	vUp = vectorUp.normalized();
+	vTo = vectorTo.normalized();
+	locationPoint3d = locatPoint;
+    }
+
+    /**
+     * @return the center of the camera: our point of view
+     */
+    public Point3D getLocationPoint3d() {
+	return locationPoint3d;
+    }
+
+    /**
+     * @return the direction where we look
+     */
+    public Vector getvTo() {
+	return vTo;
+    }
+
+    /**
+     * @return the vUp
+     */
+    public Vector getvUp() {
+	return vUp;
+    }
+
+    /**
+     * @return the vRight
+     */
+    public Vector getvRight() {
+	return vRight;
+    }
+
+    /**
+     * Method set for the size of view plane
+     * 
+     * @param width  of view plane
+     * @param height of view plane
+     * @return this (Builder pattern)
+     */
+    public Camera setViewPlaneSize(double width, double height) {
+	this.height = height;
+	this.width = width;
+	return this;
+    }
+
+    /**
+     * Set distance between our camera and the view plane
+     * 
+     * @param distance double number
+     * @return this (Builder pattern)
+     */
+    public Camera setVpDistance(double distance) {
+	this.distance = distance;
+	return this;
+    }
+
+    /**
+     * Construct Ray Through Pixel form location of camera
+     * 
+     * @param nX how much pixel we wont per row
+     * @param nY how much pixel we wont per column
+     * @param j  numbers of rows
+     * @param i  numbers of columns
+     * @return Ray form location pointing the center of a pixel
+     */
+    public Ray constructRayThroughPixel(int nX, int nY, int j, int i) {
+	Point3D Pc;
+	Pc = locationPoint3d;
+	if (!primitives.Util.isZero(distance)) {
+
+	    Pc = locationPoint3d.add(vTo.scale(distance));
+
 	}
 
-	/**
-	 * @return the locationPoint3d
-	 */
-	public Point3D getLocationPoint3d() {
-		return locationPoint3d;
+	double Ry = height / nY, Rx = width / nX;
+	double xj = (j - (nX - 1) / 2d) * Rx;
+	double yi = -(i - (nY - 1) / 2d) * Ry;
+	Point3D pij = Pc;
+	if (!primitives.Util.isZero(xj)) {
+	    pij = pij.add(vRight.scale(xj));
 	}
-
-	/**
-	 * @return the vTo 
-	 */
-	public Vector getvTo() {
-		return vTo;
+	if (!primitives.Util.isZero(yi)) {
+	    pij = pij.add(vUp.scale(yi));
 	}
-
-	/**
-	 * @return the vUp
-	 */
-	public Vector getvUp() {
-		return vUp;
-	}
-
-	/**
-	 * @return the vRight
-	 */
-	public Vector getvRight() {
-		return vRight;
-	}
-
-	/**
-	 * Method set for the size of view plane
-	 * 
-	 * @param width  of view plane
-	 * @param height of view plane
-	 * @return this (Builder pattern)
-	 */
-	public Camera setViewPlaneSize(double width, double height) {
-		this.height = height;
-		this.width = width;
-		return this;
-	}
-
-	/**
-	 * Method set for the distance view plane form camera
-	 * 
-	 * @param distance double number
-	 * @return this (Builder pattern)
-	 */
-	public Camera setVpDistance(double distance) {
-		this.distance = distance;
-		return this;
-	}
-
-	/**
-	 * construct Ray Through Pixel form location of camera
-	 * F
-	 * @param nX depend hoe pixel we wont row
-	 * @param nY depend hoe pixel we wont column
-	 * @param j Rows
-	 * @param i Columns
-	 * @return Ray form location towards the center of pixel
-	 */
-	public Ray constructRayThroughPixel(int nX, int nY, int j, int i) {
-		Point3D Pc;
-		Pc = locationPoint3d;
-		if (!primitives.Util.isZero(distance)) {
-
-			Pc = locationPoint3d.add(vTo.scale(distance));
-
-		}
-
-		double Ry = height / nY, Rx = width / nX;
-		double xj = (j - (nX - 1) / 2d) * Rx;
-		double yi = -(i - (nY - 1) / 2d) * Ry;
-		Point3D pij = Pc;
-		if (!primitives.Util.isZero(xj)) {
-			pij = pij.add(vRight.scale(xj));
-		}
-		if (!primitives.Util.isZero(yi)) {
-			pij = pij.add(vUp.scale(yi));
-		}
-		Vector vij = pij.subtract(locationPoint3d);
-		return new Ray(locationPoint3d, vij);
-	}
+	Vector vij = pij.subtract(locationPoint3d);
+	return new Ray(locationPoint3d, vij);
+    }
 
 }
