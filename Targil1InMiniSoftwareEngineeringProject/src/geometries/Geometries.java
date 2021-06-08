4f844bbf30d4c915d6dfb2baea48b45a117d8b28
alexandre lejdstrom <yehouda.lejdstrom@gmail.com>
2021-06-08 11:16:11 +0300

    added test case for getMiddlePoint method and some minor improvements

--- Targil1InMiniSoftwareEngineeringProject/src/geometries/Geometries.java
+++ Targil1InMiniSoftwareEngineeringProject/src/geometries/Geometries.java
@@ -15,159 +15,147 @@ import primitives.*;
  *
  */
 public class Geometries extends Intersectable {
-	/**
-	 * list of geometries like sphere, triangle, tube, plane,polygon, cylinder
-	 */
-	private List<Intersectable> geometries = new LinkedList<Intersectable>();
+    /**
+     * list of geometries like sphere, triangle, tube, plane,polygon, cylinder
+     */
+    private List<Intersectable> geometries = new LinkedList<Intersectable>();
 
-	/**
-	 * Default ctor for geometries
-	 */
-	public Geometries() {
+    /**
+     * Default ctor for geometries
+     */
+    public Geometries() {
+    }
+
+    /**
+     * ctor with list
+     * 
+     * @param geometries shape are implement interface Intersectable
+     */
+    public Geometries(Intersectable... geometries) {
+	add(geometries);
+    }
+
+    /**
+     * adding list
+     * 
+     * @param geometries
+     */
+    public void add(Intersectable... geometries) {
+	for (Intersectable i : geometries)
+	    this.geometries.add(i);
+    }
+
+    @Override
+    public List<GeoPoint> findGeoIntersections(Ray ray, double maxDistance) {
+	// linked because you don't now how much der is...
+	if (ray == null) return null;
+	List<GeoPoint> resultList = null;
+	for (Intersectable i : geometries) {
+	    List<GeoPoint> arr = i.findIntsersectionsBound(ray, maxDistance);
+	    if (arr != null) if (resultList == null)
+		resultList = new LinkedList<>(arr);
+	    else
+		resultList.addAll(arr);
 	}
+	return resultList;
+    }
 
-	/**
-	 * ctor with list
-	 * 
-	 * @param geometries shape are implement interface Intersectable
-	 */
-	public Geometries(Intersectable... geometries) {
-		add(geometries);
+    @Override
+    protected void CreateBoundingBox() {
+	minX = Double.MAX_VALUE;
+	minY = Double.MAX_VALUE;
+	minZ = Double.MAX_VALUE;
+	maxX = Double.MIN_VALUE;
+	maxY = Double.MIN_VALUE;
+	maxZ = Double.MIN_VALUE;
+	for (Intersectable geo : geometries) {
+	    geo.createBox();
+	    if (geo.minX < minX) minX = geo.minX;
+	    if (geo.maxX > maxX) maxX = geo.maxX;
+	    if (geo.minY < minY) minY = geo.minY;
+	    if (geo.maxY > maxY) maxY = geo.maxY;
+	    if (geo.minZ < minZ) minZ = geo.minZ;
+	    if (geo.maxZ > maxZ) maxZ = geo.maxZ;
+
 	}
+	middleBoxPoint = getMiddlePoint();
+    }
 
-	/**
-	 * adding list
-	 * 
-	 * @param geometries
-	 */
-	public void add(Intersectable... geometries) {
-		for (Intersectable i : geometries)
-			this.geometries.add(i);
+    /**
+     * the function is calling the makeTree function after taking out all infinity
+     * shapes to a separate list.<br>
+     * after the function createGeometriesTreeRecursion was called and return the
+     * binary tree list we add the infinity shape list to the head of the binary
+     * tree.<br>
+     * so geometries as all geometries in one binary tree.
+     */
+    public void createGeometriesTree() {
+	LinkedList<Intersectable> shapesWhitOutBox = null;
+	for (int i = 0; i < geometries.size(); ++i) {
+	    if (!geometries.get(i).finityShape) {
+		if (shapesWhitOutBox == null) shapesWhitOutBox = new LinkedList<Intersectable>();
+		shapesWhitOutBox.add(geometries.remove(i));
+	    }
 	}
+	geometries = createGeometriesTreeRecursion(geometries);
+	if (shapesWhitOutBox != null) geometries.addAll(0, shapesWhitOutBox);
+    }
 
-	@Override
-	public List<GeoPoint> findGeoIntersections(Ray ray, double maxDistance) {
-		// linked because you don't now how much der is...
-		if (ray == null)
-			return null;
-		List<GeoPoint> resultList = null;
-		for (Intersectable i : geometries) {
-			List<GeoPoint> arr = i.findIntsersectionsBound(ray, maxDistance);
-			if (arr != null)
-				if (resultList == null)
-					resultList = new LinkedList<>(arr);
-				else
-					resultList.addAll(arr);
+    /**
+     * update box size after every new geometry we add to geometries list. create
+     * the box that include both bodies
+     */
+    protected void updateBoxSize(Intersectable a, Intersectable b) {
+	finityShape = true;
+	minX = Double.MAX_VALUE;
+	minY = Double.MAX_VALUE;
+	minZ = Double.MAX_VALUE;
+	maxX = Double.MIN_VALUE;
+	maxY = Double.MIN_VALUE;
+	maxZ = Double.MIN_VALUE;
+	minX = Math.min(a.minX, b.minX);
+	minY = Math.min(a.minY, b.minY);
+	minZ = Math.min(a.minZ, b.minZ);
+	maxX = Math.max(a.maxX, b.maxX);
+	maxY = Math.max(a.maxY, b.maxY);
+	maxZ = Math.max(a.maxZ, b.maxZ);
+	middleBoxPoint = getMiddlePoint();
+    }
+
+    /**
+     * The function calculates the list that represents a binary tree of boxes
+     * within boxes...<br>
+     * inside each box there are two closely related shapes,<br>
+     * used to accelerate the ray tracer,<br>
+     * in case a ray does not cut the box all the boxes inside it are probably not
+     * cut.
+     *
+     * @param finiteShapes the list of finite Shapes
+     * @return a list of shapes present tree of geometries
+     */
+    List<Intersectable> createGeometriesTreeRecursion(List<Intersectable> finiteShapes) {
+	if (finiteShapes.size() == 1) return finiteShapes;
+	LinkedList<Intersectable> _newShapes = null;
+	while (!finiteShapes.isEmpty()) {
+	    Intersectable first = finiteShapes.remove(0), nextTo = finiteShapes.get(0);
+	    double minD = first.middleBoxPoint.distance(nextTo.middleBoxPoint);
+	    int min = 0;
+	    for (int i = 1; i < finiteShapes.size(); ++i) {
+		if (minD == 0) break;
+		double temp = first.middleBoxPoint.distance(finiteShapes.get(i).middleBoxPoint);
+		if (temp < minD) {
+		    minD = temp;
+		    nextTo = finiteShapes.get(i);
+		    min = i;
 		}
-		return resultList;
+	    }
+	    if (_newShapes == null) _newShapes = new LinkedList<Intersectable>();
+	    finiteShapes.remove(min);
+	    Geometries newGeo = new Geometries(first, nextTo);
+	    newGeo.updateBoxSize(first, nextTo);
+	    _newShapes.add(newGeo);
+	    if (finiteShapes.size() == 1) _newShapes.add(finiteShapes.remove(0));
 	}
-	@Override
-	protected void CreateBoundingBox() {
-		minX = Double.MAX_VALUE;
-		minY = Double.MAX_VALUE;
-		minZ = Double.MAX_VALUE;
-		maxX = Double.MIN_VALUE;
-		maxY = Double.MIN_VALUE;
-		maxZ = Double.MIN_VALUE;
-		for (Intersectable geo : geometries) {
-			geo.createBox();
-			if (geo.minX < minX)
-				minX = geo.minX;
-			if (geo.maxX > maxX)
-				maxX = geo.maxX;
-			if (geo.minY < minY)
-				minY = geo.minY;
-			if (geo.maxY > maxY)
-				maxY = geo.maxY;
-			if (geo.minZ < minZ)
-				minZ = geo.minZ;
-			if (geo.maxZ > maxZ)
-				maxZ = geo.maxZ;
-
-		}
-		middleBoxPoint = getMiddlePoint();
-	}
-
-	/**
-	 * the function is calling the makeTree function after taking out all infinity
-	 * shapes to a separate list.<br>
-	 * after the function createGeometriesTreeRecursion was called and return the
-	 * binary tree list we add the infinity shape list to the head of the binary
-	 * tree.<br>
-	 * so geometries as all geometries in one binary tree.
-	 */
-	public void createGeometriesTree() {
-		LinkedList<Intersectable> shapesWhitOutBox = null;
-		for (int i = 0; i < geometries.size(); ++i) {
-			if (!geometries.get(i).finityShape) {
-				if (shapesWhitOutBox == null)
-					shapesWhitOutBox = new LinkedList<Intersectable>();
-				shapesWhitOutBox.add(geometries.remove(i));
-			}
-		}
-		geometries = createGeometriesTreeRecursion(geometries);
-		if (shapesWhitOutBox != null)
-			geometries.addAll(0, shapesWhitOutBox);
-	}
-
-	/**
-	 * update box size after every new geometry we add to geometries list.
-	 */
-	protected void updateBoxSize(Intersectable a, Intersectable b) {
-		finityShape = true;
-		minX = Double.MAX_VALUE;
-		minY = Double.MAX_VALUE;
-		minZ = Double.MAX_VALUE;
-		maxX = Double.MIN_VALUE;
-		maxY = Double.MIN_VALUE;
-		maxZ = Double.MIN_VALUE;
-		minX = Math.min(a.minX, b.minX);
-		minY = Math.min(a.minY, b.minY);
-		minZ = Math.min(a.minZ, b.minZ);
-		maxX = Math.max(a.maxX, b.maxX);
-		maxY = Math.max(a.maxY, b.maxY);
-		maxZ = Math.max(a.maxZ, b.maxZ);
-		middleBoxPoint = getMiddlePoint();
-	}
-
-	/**
-	 * The function calculates the list that represents a binary tree of boxes
-	 * within boxes...<br>
-	 * inside each box there are two closely related shapes,<br>
-	 * used to accelerate the ray tracer,<br>
-	 * in case a ray does not cut the box all the boxes inside it are probably not
-	 * cut.
-	 *
-	 * @param finiteShapes the list of finite Shapes
-	 * @return a list of shapes present tree of geometries
-	 */
-	List<Intersectable> createGeometriesTreeRecursion(List<Intersectable> finiteShapes) {
-		if (finiteShapes.size() == 1)
-			return finiteShapes;
-		LinkedList<Intersectable> _newShapes = null;
-		while (!finiteShapes.isEmpty()) {
-			Intersectable first = finiteShapes.remove(0), nextTo = finiteShapes.get(0);
-			double minD = first.middleBoxPoint.distance(nextTo.middleBoxPoint);
-			int min = 0;
-			for (int i = 1; i < finiteShapes.size(); ++i) {
-				if (minD == 0)
-					break;
-				double temp = first.middleBoxPoint.distance(finiteShapes.get(i).middleBoxPoint);
-				if (temp < minD) {
-					minD = temp;
-					nextTo = finiteShapes.get(i);
-					min = i;
-				}
-			}
-			if (_newShapes == null)
-				_newShapes = new LinkedList<Intersectable>();
-			finiteShapes.remove(min);
-			Geometries newGeo = new Geometries(first, nextTo);
-			newGeo.updateBoxSize(first, nextTo);
-			_newShapes.add(newGeo);
-			if (finiteShapes.size() == 1)
-				_newShapes.add(finiteShapes.remove(0));
-		}
-		return createGeometriesTreeRecursion(_newShapes);
-	}
+	return createGeometriesTreeRecursion(_newShapes);
+    }
 }
--- Targil1InMiniSoftwareEngineeringProject/src/unittests/geometries/SphereTests.java
+++ Targil1InMiniSoftwareEngineeringProject/src/unittests/geometries/SphereTests.java
@@ -17,101 +17,111 @@ import primitives.*;
  *
  */
 public class SphereTests {
-	/**
-	 * Test method for {@link geometries.Sphere#getNormal(primitives.Point3D)}.
-	 */
-	@Test
-	public void testGetNormal() {
-		// ============ Equivalence Partitions Tests ==============
-		// TC01: simple test
-		Sphere s = new Sphere(new Point3D(0, 0, 1), 1);
-		Vector v1 = s.getNormal(new Point3D(0, 0, 0));
-		// The expected normal is (0,0,-1)
-		assertEquals("The Sphere normal calculation is incorrect1", new Vector(0, 0, -1), v1);
-	}
+    /**
+     * Test method for {@link geometries.Sphere#getNormal(primitives.Point3D)}.
+     */
+    @Test
+    public void testGetNormal() {
+	// ============ Equivalence Partitions Tests ==============
+	// TC01: simple test
+	Sphere s = new Sphere(new Point3D(0, 0, 1), 1);
+	Vector v1 = s.getNormal(new Point3D(0, 0, 0));
+	// The expected normal is (0,0,-1)
+	assertEquals("The Sphere normal calculation is incorrect1", new Vector(0, 0, -1), v1);
+    }
 
-	/**
-	 * Test method for {@link geometries.Sphere#findIntersections(primitives.Ray)}.
-	 */
+    /**
+     * Test method for {@link geometries.Sphere#findIntersections(primitives.Ray)}.
+     */
 
-	@Test
-	public void testFindIntersections() {
+    @Test
+    public void testFindIntersections() {
 
-		Sphere sphere = new Sphere(new Point3D(1, 0, 0), 1d);
-		Sphere sphere1 = new Sphere(new Point3D(2, 0, 0), 1);
-		// ============ Equivalence Partitions Tests==============
+	Sphere sphere = new Sphere(new Point3D(1, 0, 0), 1d);
+	Sphere sphere1 = new Sphere(new Point3D(2, 0, 0), 1);
+	// ============ Equivalence Partitions Tests==============
 
-		// TC01: Ray's line is outside the sphere (0 points)
-		assertEquals("Ray's line out of sphere", null,
-				sphere.findIntersections(new Ray(new Point3D(-1, 0, 0), new Vector(1, 1, 0))));
+	// TC01: Ray's line is outside the sphere (0 points)
+	assertEquals("Ray's line out of sphere", null,
+		sphere.findIntersections(new Ray(new Point3D(-1, 0, 0), new Vector(1, 1, 0))));
 
-		// TC02: Ray starts before and crosses the sphere (2 points)
-		Point3D p1 = new Point3D(0.0651530771650466, 0.355051025721682, 0);
-		Point3D p2 = new Point3D(1.53484692283495, 0.844948974278318, 0);
-		List<Point3D> result = sphere.findIntersections(new Ray(new Point3D(-1, 0, 0), new Vector(3, 1, 0)));
-		assertEquals("Wrong number of points", 2, result.size());
-		if (result.get(0).getX() > result.get(1).getX())
-			result = List.of(result.get(1), result.get(0));
-		assertEquals("Ray crosses sphere", List.of(p1, p2), result);
-		// TC03: Ray starts inside the sphere (1 point)
-		List<Point3D> result0 = sphere1.findIntersections(new Ray(new Point3D(1.5, 0, 0.5), new Vector(-1, 0, -1)));
-		assertEquals("Wrong number of points", 1, result0.size());
-		assertEquals("Ray starts inside sphere", List.of(new Point3D(1, 0, 0)), result0);
-		// TC04: Ray starts after the sphere (0 points)
-		List<Point3D> result1 = sphere.findIntersections(new Ray(new Point3D(-2, 0, 0), new Vector(-1, 0, 0)));
-		assertEquals("Ray starts after sphere", null, result1);
+	// TC02: Ray starts before and crosses the sphere (2 points)
+	Point3D p1 = new Point3D(0.0651530771650466, 0.355051025721682, 0);
+	Point3D p2 = new Point3D(1.53484692283495, 0.844948974278318, 0);
+	List<Point3D> result = sphere.findIntersections(new Ray(new Point3D(-1, 0, 0), new Vector(3, 1, 0)));
+	assertEquals("Wrong number of points", 2, result.size());
+	if (result.get(0).getX() > result.get(1).getX()) result = List.of(result.get(1), result.get(0));
+	assertEquals("Ray crosses sphere", List.of(p1, p2), result);
+	// TC03: Ray starts inside the sphere (1 point)
+	List<Point3D> result0 = sphere1.findIntersections(new Ray(new Point3D(1.5, 0, 0.5), new Vector(-1, 0, -1)));
+	assertEquals("Wrong number of points", 1, result0.size());
+	assertEquals("Ray starts inside sphere", List.of(new Point3D(1, 0, 0)), result0);
+	// TC04: Ray starts after the sphere (0 points)
+	List<Point3D> result1 = sphere.findIntersections(new Ray(new Point3D(-2, 0, 0), new Vector(-1, 0, 0)));
+	assertEquals("Ray starts after sphere", null, result1);
 
-		// =============== Boundary Values Tests ==================
+	// =============== Boundary Values Tests ==================
 
-		// **** Group: Ray's line crosses the sphere (but not the center)
-		// TC11:Ray starts at sphere and goes inside (1 points)
-		List<Point3D> result2 = sphere.findIntersections(new Ray(new Point3D(1, 0, 1), new Vector(-1, 0, -1)));
-		assertEquals("crosses the sphere :Ray starts at sphere and goes inside", List.of(new Point3D(0, 0, 0)),
-				result2);
-		// TC12: Ray starts at sphere and goes outside(0 points)
-		List<Point3D> result3 = sphere.findIntersections(new Ray(new Point3D(1, 0, 1), new Vector(-1, 0, 0.5)));
-		assertEquals("crosses the sphere :Ray starts at sphere and goes outside", null, result3);
+	// **** Group: Ray's line crosses the sphere (but not the center)
+	// TC11:Ray starts at sphere and goes inside (1 points)
+	List<Point3D> result2 = sphere.findIntersections(new Ray(new Point3D(1, 0, 1), new Vector(-1, 0, -1)));
+	assertEquals("crosses the sphere :Ray starts at sphere and goes inside", List.of(new Point3D(0, 0, 0)),
+		result2);
+	// TC12: Ray starts at sphere and goes outside(0 points)
+	List<Point3D> result3 = sphere.findIntersections(new Ray(new Point3D(1, 0, 1), new Vector(-1, 0, 0.5)));
+	assertEquals("crosses the sphere :Ray starts at sphere and goes outside", null, result3);
 
-		// **** Group: Ray's line goes through the center*****
-		// TC13: Ray starts before the sphere (2 points)
-		List<Point3D> result4 = sphere1
-				.findIntersections(new Ray(new Point3D(0.5, 0, 0), new Vector(new Point3D(3, 0, 0))));
-		assertEquals("Wrong number of points", 2, result4.size());
-		if (result.get(0).getX() > result.get(1).getX())
-			result4 = List.of(result.get(1), result.get(0));
-		assertEquals("through the center:Ray starts before the sphere",
-				List.of(new Point3D(1, 0, 0), new Point3D(3, 0, 0)), result4);
-		// TC14: Ray starts at sphere and goes inside (1 points)
-		List<Point3D> result5 = sphere1.findIntersections(new Ray(new Point3D(1, 0, 0), new Vector(3, 0, 0)));
-		assertEquals("through the center:starts at sphere and goes inside", List.of(new Point3D(3, 0, 0)), result5);
-		// TC15: Ray starts inside (1 points)
-		List<Point3D> result6 = sphere1.findIntersections(new Ray(new Point3D(1.5, 0, 0), new Vector(3, 0, 0)));
-		assertEquals("through the center:Ray starts inside", List.of(new Point3D(3, 0, 0)), result6);
-		// TC16: Ray starts at the center (1 points)
-		List<Point3D> result7 = sphere1.findIntersections(new Ray(new Point3D(2, 0, 0), new Vector(3, 0, 0)));
-		assertEquals("through the center:Ray starts at the center", List.of(new Point3D(3, 0, 0)), result7);
-		// TC17: Ray starts at sphere and goes outside(0 points)
-		List<Point3D> result8 = sphere1.findIntersections(new Ray(new Point3D(3, 0, 0), new Vector(1, 0, 0)));
-		assertEquals("through the center:Ray starts at sphere and goes outside", null, result8);
-		// TC18: Ray starts after sphere (0 points)
-		List<Point3D> result9 = sphere1.findIntersections(new Ray(new Point3D(3.5, 0, 0), new Vector(1, 0, 0)));
-		assertEquals("through the center:starts after sphere", null, result9);
+	// **** Group: Ray's line goes through the center*****
+	// TC13: Ray starts before the sphere (2 points)
+	List<Point3D> result4 = sphere1
+		.findIntersections(new Ray(new Point3D(0.5, 0, 0), new Vector(new Point3D(3, 0, 0))));
+	assertEquals("Wrong number of points", 2, result4.size());
+	if (result.get(0).getX() > result.get(1).getX()) result4 = List.of(result.get(1), result.get(0));
+	assertEquals("through the center:Ray starts before the sphere",
+		List.of(new Point3D(1, 0, 0), new Point3D(3, 0, 0)), result4);
+	// TC14: Ray starts at sphere and goes inside (1 points)
+	List<Point3D> result5 = sphere1.findIntersections(new Ray(new Point3D(1, 0, 0), new Vector(3, 0, 0)));
+	assertEquals("through the center:starts at sphere and goes inside", List.of(new Point3D(3, 0, 0)), result5);
+	// TC15: Ray starts inside (1 points)
+	List<Point3D> result6 = sphere1.findIntersections(new Ray(new Point3D(1.5, 0, 0), new Vector(3, 0, 0)));
+	assertEquals("through the center:Ray starts inside", List.of(new Point3D(3, 0, 0)), result6);
+	// TC16: Ray starts at the center (1 points)
+	List<Point3D> result7 = sphere1.findIntersections(new Ray(new Point3D(2, 0, 0), new Vector(3, 0, 0)));
+	assertEquals("through the center:Ray starts at the center", List.of(new Point3D(3, 0, 0)), result7);
+	// TC17: Ray starts at sphere and goes outside(0 points)
+	List<Point3D> result8 = sphere1.findIntersections(new Ray(new Point3D(3, 0, 0), new Vector(1, 0, 0)));
+	assertEquals("through the center:Ray starts at sphere and goes outside", null, result8);
+	// TC18: Ray starts after sphere (0 points)
+	List<Point3D> result9 = sphere1.findIntersections(new Ray(new Point3D(3.5, 0, 0), new Vector(1, 0, 0)));
+	assertEquals("through the center:starts after sphere", null, result9);
 
-		// **** Group: Ray's line is tangent to the sphere (all tests 0 points)
-		// TC19: Ray starts before the tangent point
-		List<Point3D> result10 = sphere.findIntersections(new Ray(new Point3D(0, 0, 0.5), new Vector(0, 0, 1)));
-		assertEquals("Ray's line is tangent :Ray starts before the tangent point", null, result10);
-		// TC20: Ray starts at the tangent point
-		List<Point3D> result11 = sphere.findIntersections(new Ray(new Point3D(2, 0, 0), new Vector(0, 1, 0)));
-		assertEquals("Ray's line is tangent :Ray starts at the tangent point", null, result11);
-		// TC21: Ray starts after the tangent point
-		List<Point3D> result12 = sphere.findIntersections(new Ray(new Point3D(2, -1, 0), new Vector(1, 0, 0)));
-		assertEquals("Ray's line is tangent :Ray starts after the tangent point", null, result12);
-		// **** Group: Special cases *******
-		// TC19: Ray's line is outside, ray is orthogonal
-		// to ray start to sphere's center line
-		List<Point3D> result13 = sphere.findIntersections(new Ray(new Point3D(2.5, 0, 0), new Vector(0, 1, 0)));
-		assertEquals("ray is orthogonal and ray start to sphere's center line", null, result13);
-	}
+	// **** Group: Ray's line is tangent to the sphere (all tests 0 points)
+	// TC19: Ray starts before the tangent point
+	List<Point3D> result10 = sphere.findIntersections(new Ray(new Point3D(0, 0, 0.5), new Vector(0, 0, 1)));
+	assertEquals("Ray's line is tangent :Ray starts before the tangent point", null, result10);
+	// TC20: Ray starts at the tangent point
+	List<Point3D> result11 = sphere.findIntersections(new Ray(new Point3D(2, 0, 0), new Vector(0, 1, 0)));
+	assertEquals("Ray's line is tangent :Ray starts at the tangent point", null, result11);
+	// TC21: Ray starts after the tangent point
+	List<Point3D> result12 = sphere.findIntersections(new Ray(new Point3D(2, -1, 0), new Vector(1, 0, 0)));
+	assertEquals("Ray's line is tangent :Ray starts after the tangent point", null, result12);
+	// **** Group: Special cases *******
+	// TC19: Ray's line is outside, ray is orthogonal
+	// to ray start to sphere's center line
+	List<Point3D> result13 = sphere.findIntersections(new Ray(new Point3D(2.5, 0, 0), new Vector(0, 1, 0)));
+	assertEquals("ray is orthogonal and ray start to sphere's center line", null, result13);
+    }
+
+    /**
+     * test getMiddlePoint method
+     */
+    @Test
+    public void testGetMiddlePoint() {
+	Sphere sphere = new Sphere(new Point3D(5, 5, 5), 3);
+	sphere.createBox();
+
+	// TC01: middle point is the center of the sphere
+	assertEquals("the middle point expected", new Point3D(5, 5, 5), sphere.getMiddlePoint());
+    }
 
 }
